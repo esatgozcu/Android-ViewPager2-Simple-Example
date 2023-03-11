0da2b9f92eee1a11b42c3309e2e4c4b1d14f35c9
Esat GÖZCÜ <esatgozcu@gmail.com>
2023-03-11 21:00:14 +1000

    0.0.2
    Binding and navigation have been added

--- app/src/main/java/com/esatgozcu/viewpager2simpleexample/MainActivity.kt
+++ app/src/main/java/com/esatgozcu/viewpager2simpleexample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.esatgozcu.viewpager2simpleexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.esatgozcu.viewpager2simpleexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
